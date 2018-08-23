a962a3e87fad4da89f12959aa21f8d6d43ade777
sfcorenan <33206543+sfcorenan@users.noreply.github.com>
2018-08-23 17:14:41 -0300

    Add files via upload

---
+++ Instrumentação LAERO/testeee/testeee.ino
@@ -0,0 +1,48 @@
+
+#include <NTPClient.h>
+#include <WiFi.h>
+#include <WiFiUdp.h>
+ 
+const char *ssid     = "AERO";
+const char *password = "aerodromo";
+ 
+WiFiUDP ntpUDP;
+ 
+int16_t utc = -3; //UTC -3:00 Brazil
+uint32_t currentMillis = 0;
+uint32_t previousMillis = 0;
+ 
+NTPClient timeClient(ntpUDP, "a.st1.ntp.br", utc*3600, 60000);
+ 
+void setup(){
+  Serial.begin(115200);
+ 
+  WiFi.begin(ssid, password);
+ 
+  while ( WiFi.status() != WL_CONNECTED ) {
+    delay ( 500 );
+    Serial.print ( "." );
+  }
+ 
+  timeClient.begin();
+  timeClient.update();
+}
+ 
+void forceUpdate(void) {
+  timeClient.forceUpdate();
+}
+ 
+void checkOST(void) {
+  currentMillis = millis();//Tempo atual em ms
+  //Lógica de verificação do tempo
+  if (currentMillis - previousMillis > 1000) {
+    previousMillis = currentMillis;    // Salva o tempo atual
+    printf("Time Epoch: %d: ", timeClient.getEpochTime());
+    Serial.println(timeClient.getFormattedTime());
+  }
+}
+ 
+void loop() {
+  //Chama a verificacao de tempo
+  checkOST();
+}
